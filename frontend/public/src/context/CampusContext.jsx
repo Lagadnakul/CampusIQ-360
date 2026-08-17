859668b4bdaead605494d2ce62bde061bc84a860
Lagadnakul <nakullagad084@gmail.com>
2026-08-17 22:21:51 +0530

    push

--- frontend/public/src/context/CampusContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const CampusContext = createContext(null);
-
-export default CampusContext;
--- frontend/public/src/context/MonitoringContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const MonitoringContext = createContext(null);
-
-export default MonitoringContext;
