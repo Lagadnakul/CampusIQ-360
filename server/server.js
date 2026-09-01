ac6d07096148b60fb0892fb1c4940a0c6ddd50b6
Lagadnakul <nakullagad084@gmail.com>
2026-09-01 16:04:39 +0530

    push

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-const app = require("./src/app");
+require("dotenv").config();
 
-const PORT = 5000;
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
-    console.log(`CampusIQ 360 server running on port ${PORT}`)
+  console.log(`CampusIQ 360 server running on port ${PORT}`);
 });
